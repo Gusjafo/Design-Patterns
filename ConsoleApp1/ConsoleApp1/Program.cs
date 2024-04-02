c9b0913c49792ec4f79e8b54024f445bdff26621
Gustavo Fonollosa <gustavojafo@gmail.com>
2024-04-02 11:06:47 -0300

    Add factory pattern

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using Builder;
 using Singleton;
+using Factory;
 
 namespace MainSolution
 {
@@ -14,54 +15,60 @@ namespace MainSolution
              * Builder Pattern
              */
 
-            // The client code creates a builder object, passes it to the
-            // director and then initiates the construction process. The end
-            // result is retrieved from the builder object.
-            var director = new Director();
-            var builder = new ConcreteBuilder();
-            director.Builder = builder;
+            //// The client code creates a builder object, passes it to the
+            //// director and then initiates the construction process. The end
+            //// result is retrieved from the builder object.
+            //var director = new Director();
+            //var builder = new ConcreteBuilder();
+            //director.Builder = builder;
 
-            Console.WriteLine("Standard basic product:");
-            director.BuildMinimalViableProduct();
-            Console.WriteLine(builder.GetProduct().ListParts());
+            //Console.WriteLine("Standard basic product:");
+            //director.BuildMinimalViableProduct();
+            //Console.WriteLine(builder.GetProduct().ListParts());
 
-            Console.WriteLine("Standard full featured product:");
-            director.BuildFullFeaturedProduct();
-            Console.WriteLine(builder.GetProduct().ListParts());
+            //Console.WriteLine("Standard full featured product:");
+            //director.BuildFullFeaturedProduct();
+            //Console.WriteLine(builder.GetProduct().ListParts());
 
-            // Remember, the Builder pattern can be used without a Director
-            // class.
-            Console.WriteLine("Custom product:");
-            builder.BuildPartA();
-            builder.BuildPartC();
-            Console.Write(builder.GetProduct().ListParts());
+            //// Remember, the Builder pattern can be used without a Director
+            //// class.
+            //Console.WriteLine("Custom product:");
+            //builder.BuildPartA();
+            //builder.BuildPartC();
+            //Console.Write(builder.GetProduct().ListParts());
 
             ////////////////////////////////////////////////////////////
             /*
              * Singleton Pattern
              */
 
-            Console.WriteLine(
-               "{0}\n{1}\n\n{2}\n",
-               "If you see the same value, then singleton was reused (yay!)",
-               "If you see different values, then 2 singletons were created (booo!!)",
-               "RESULT:"
-           );
+            // Console.WriteLine(
+            //    "{0}\n{1}\n\n{2}\n",
+            //    "If you see the same value, then singleton was reused (yay!)",
+            //    "If you see different values, then 2 singletons were created (booo!!)",
+            //    "RESULT:"
+            //);
 
-            Thread process1 = new Thread(() =>
-            {
-                TestSingleton("FOO");
-            });
-            Thread process2 = new Thread(() =>
-            {
-                TestSingleton("BAR");
-            });
+            // Thread process1 = new Thread(() =>
+            // {
+            //     TestSingleton("FOO");
+            // });
+            // Thread process2 = new Thread(() =>
+            // {
+            //     TestSingleton("BAR");
+            // });
 
-            process1.Start();
-            process2.Start();
+            // process1.Start();
+            // process2.Start();
 
-            process1.Join();
-            process2.Join();
+            // process1.Join();
+            // process2.Join();
+
+            ////////////////////////////////////////////////////////////
+            /*
+             * Factory Pattern
+             */
+            new Client().Main();
         }
 
         public static void TestSingleton(string value)
